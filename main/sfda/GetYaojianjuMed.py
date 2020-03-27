05a9a41c2c4027cc7f67831e2ccda3d92d98aed6
fengjicheng <fengjicheng@jd.com>
2020-03-27 18:02:32 +0800

    药监局药品爬虫

--- main/sfda/GetYaojianjuMed.py
+++ main/sfda/GetYaojianjuMed.py
@@ -12,43 +12,72 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def get_yjj_med_id(url):
-    file = open('txt/yjj_med_id.txt', 'a', encoding='utf-8')
-    path = 'C:\chromedriver\chromedriver.exe'
-    option = webdriver.ChromeOptions()
-    option.add_argument('headless')
-    browser = webdriver.Chrome(executable_path=path, chrome_options=option)
+class GetMedID(object):
+    def __init__(self, url, page_num, file, fail_file):
+        self.url = url
+        self.page_num = page_num
+        self.file = file
+        self.fail_file = fail_file
 
-    try:
-        browser.get(url)
-        wait = WebDriverWait(browser, 10)
-        elem_list = browser.find_elements_by_xpath('//table[position()=2]//a')
-        if elem_list:
-            for elem in elem_list:
-                href = elem.get_attribute('href')
-                sep1 = re.compile(r"Id=(\d+)'")
-                num = sep1.findall(href)
-                if num:
-                    file.write(num[0] + '\n')
-                    file.flush()
-    finally:
-        browser.close()
+    def get_yjj_med_id(self, url):
+        file = open(self.file, 'a', encoding='utf-8')
+        path = 'C:\chromedriver\chromedriver.exe'
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        browser = webdriver.Chrome(executable_path=path, chrome_options=option)
+        # browser = webdriver.Chrome(executable_path=path)
+
+        try:
+            browser.get(url)
+            wait = WebDriverWait(browser, 10)
+            elem_list = browser.find_elements_by_xpath('//table[position()=2]//a')
+            if elem_list:
+                for elem in elem_list:
+                    href = elem.get_attribute('href')
+                    sep1 = re.compile(r"Id=(\d+)'")
+                    num = sep1.findall(href)
+                    if num:
+                        file.write(num[0] + '\n')
+                        file.flush()
+            print(browser.page_source)
+        finally:
+            browser.close()
+            browser.quit()
+            file.close()
+            pass
+
+
+    def get_all_yjj_med_id(self):
+        file =open(self.fail_file, 'a', encoding='utf-8')
+        for page in tqdm(range(1, self.page_num)):  #
+            try:
+                self.get_yjj_med_id(self.url + str(page))
+                # print('++++++++++++第 {} 页爬取完成++++++++++++'.format(page))
+            except Exception:
+                file.write(str(page) + '\n')
+                file.flush()
+                print('------------第 {} 页爬取失败------------'.format(page))
+            # time.sleep(1)
         file.close()
 
-
-def get_all_yjj_med_id():
-    file =open('txt/fail_yjj_med_id.txt', 'a', encoding='utf-8')
-    for page in tqdm(range(5464, 11053)):  # 5463
-        url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/search.jsp?tableId=25&State=1&bcId=152904713761213296322795806604&tableName=TABLE25&viewtitleName=COLUMN167&viewsubTitleName=COLUMN821,COLUMN170,COLUMN166&curstart='
-        try:
-            get_yjj_med_id(url + str(page))
-            # print('++++++++++++第 {} 页爬取完成++++++++++++'.format(page))
-        except Exception:
-            file.write(str(page) + '\n')
-            file.flush()
-            print('------------第 {} 页爬取失败------------'.format(page))
-        # time.sleep(1)
-    file.close()
-
 if __name__ == '__main__':
-    get_all_yjj_med_id()
+    # # 国产药品
+    # url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/search.jsp?tableId=25&State=1&bcId=152904713761213296322795806604&tableName=TABLE25&viewtitleName=COLUMN167&viewsubTitleName=COLUMN821,COLUMN170,COLUMN166&curstart='
+    # page_num = 11044
+    # file = 'txt/yjj_med_id.txt'
+    # fail_file = 'txt/fail_yjj_med_id.txt'
+
+    # 国家基本药物（2018年版）
+    url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/search.jsp?tableId=138&State=1&bcId=152911951192978460689645865168&tableName=TABLE138&viewtitleName=COLUMN1879&viewsubTitleName=COLUMN1876,COLUMN1878,COLUMN1877&curstart='
+    page_num = 2  # 47
+    file = 'txt/yjj_jb_med_id.txt'
+    fail_file = 'txt/fail_yjj_jb_med_id.txt'
+
+    # # 麻醉药品和精神药品品种目录
+    # url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/search.jsp?tableId=102&State=1&bcId=152911939788030557312353847510&tableName=TABLE102&viewtitleName=COLUMN1349&viewsubTitleName=COLUMN1353,COLUMN1350&curstart='
+    # page_num = 27
+    # file = 'txt/yjj_mz_med_id.txt'
+    # fail_file = 'txt/fail_yjj_mz_med_id.txt'
+
+    obj = GetMedID(url, page_num, file, fail_file)
+    obj.get_all_yjj_med_id()
--- main/sfda/GetYaojianjuMedDetail.py
+++ main/sfda/GetYaojianjuMedDetail.py
@@ -10,55 +10,74 @@ import threading
 from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-from main.common.mongo.MongoWriteRead import *
+from yiyao_data_crawl.main.common.mongo.MongoWriteRead import *
 
 
-def get_yjj_med_detail(url):
-    path = 'C:\chromedriver\chromedriver.exe'
-    option = webdriver.ChromeOptions()
-    option.add_argument('headless')
-    browser = webdriver.Chrome(executable_path=path, chrome_options=option)
-    try:
-        browser.get(url)
-        wait = WebDriverWait(browser, 10)
-        tr1_list = browser.find_elements_by_xpath('//table[position()=1]//tr[position()>1]//td[position()=1]')
-        tr2_list = browser.find_elements_by_xpath('//table[position()=1]//tr[position()>1]//td[position()=2]')
-        med_dict = {}
-        if tr1_list and tr2_list:
-            for td1 in tr1_list:
-                key = td1.text.strip()
-                value = tr2_list[tr1_list.index(td1)].text.strip()
-                if key:
-                    med_dict[key] = value
-        insert('yaojianju', med_dict)
-    finally:
-        browser.close()
-        browser.quit()
+class GetMedDetail(object):
+    def __init__(self, url, file, db_name):
+        self.url = url
+        self.file = file
 
-def get_all_yjj_med_detail():
-    with open('txt/yjj_med_id.txt', 'r', encoding='utf-8') as f:
-        for num in tqdm(f.readlines()):
-            url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/content.jsp?tableId=25&tableName=TABLE25&Id=' + str(num.strip())
-            get_yjj_med_detail(url)
+    def get_yjj_med_detail(self, url):
+        path = 'C:\chromedriver\chromedriver.exe'
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        browser = webdriver.Chrome(executable_path=path, chrome_options=option)
+        try:
+            browser.get(url)
+            wait = WebDriverWait(browser, 10)
+            tr1_list = browser.find_elements_by_xpath('//table[position()=1]//tr[position()>1]//td[position()=1]')
+            tr2_list = browser.find_elements_by_xpath('//table[position()=1]//tr[position()>1]//td[position()=2]')
+            med_dict = {}
+            if tr1_list and tr2_list:
+                for td1 in tr1_list:
+                    key = td1.text.strip()
+                    value = tr2_list[tr1_list.index(td1)].text.strip()
+                    if key:
+                        med_dict[key] = value
+            insert(db_name, med_dict)
+        finally:
+            browser.close()
+            browser.quit()
 
-def get_all_yjj_med_detail_thread():
-    threads = []
-    with open('txt/yjj_med_id.txt', 'r', encoding='utf-8') as f:
-        for i in f.readlines():
-            url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/content.jsp?tableId=25&tableName=TABLE25&Id=' + str(i.strip())
-            t = threading.Thread(target=get_yjj_med_detail, args=(url,))
-            threads.append(t)
-        num = range(len(threads))
-        n = 10
-        for x in tqdm([num[i:i + n] for i in range(0, len(num), n)]):
-            for y in x:
-                threads[y].start()  # 执行线程的start方法，线程开始执行
+    def get_all_yjj_med_detail(self):
+        with open(self.file, 'r', encoding='utf-8') as f:
+            for num in tqdm(f.readlines()):
+                url = 'self.url' + str(num.strip())
+                self.get_yjj_med_detail(url)
 
-            for y in x:
-                threads[y].join()  # 这行线程的join方法，等待线程结束，如果主进程不需要等待线程结束，可以不需要调用join方法。
+    def get_all_yjj_med_detail_thread(self):
+        threads = []
+        with open('txt/yjj_med_id.txt', 'r', encoding='utf-8') as f:
+            for i in f.readlines():
+                url = 'self.url' + str(i.strip())
+                t = threading.Thread(target=self.get_yjj_med_detail, args=(url,))
+                threads.append(t)
+            num = range(len(threads))
+            n = 10
+            for x in tqdm([num[i:i + n] for i in range(0, len(num), n)]):
+                for y in x:
+                    threads[y].start()  # 执行线程的start方法，线程开始执行
+
+                for y in x:
+                    threads[y].join()  # 这行线程的join方法，等待线程结束，如果主进程不需要等待线程结束，可以不需要调用join方法。
 
 
 if __name__ == '__main__':
-    # get_all_yjj_med_detail()
-    get_all_yjj_med_detail_thread()
+    # # 国产药品
+    # url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/content.jsp?tableId=25&tableName=TABLE25&Id='
+    # file = 'txt/yjj_med_id.txt'
+    # db_name = yaojianju
+
+    # 国家基本药物（2018年版）
+    url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/content.jsp?tableId=138&tableName=TABLE138&Id='
+    file = 'txt/yjj_jb_med_id.txt'
+    db_name = 'yaojianju_jb'
+
+    # # 麻醉药品和精神药品品种目录
+    # url = 'http://qy1.sfda.gov.cn/datasearchcnda/face3/content.jsp?tableId=102&tableName=TABLE102&Id='
+    # file = 'txt/yjj_mz_med_id.txt'
+    # db_name = 'yaojianju_mz
+    obj = GetMedDetail(url, file, db_name)
+    obj.get_all_yjj_med_detail_thread()
 
